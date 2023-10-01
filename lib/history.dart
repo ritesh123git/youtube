013da33c63464c3c38dbd55eb74fca9c76a0ce23
Ritesh jha <111743207+ritesh123git@users.noreply.github.com>
2023-10-02 00:44:59 +0530

    Update history.dart
    updated code keep tarck of changes

--- lib/history.dart
+++ lib/history.dart
@@ -6,6 +6,18 @@ class History extends StatelessWidget{
 
   @override
   Widget build(context){
-    return const Center(child: Text("History page"));
+    return Column(
+      children:[
+        const Text("video 1"),
+        const SizedBox(height : 10),
+        const Text("video 2"),
+        const SizedBox(height : 10),
+        const Text("video 3"),
+        const SizedBox(height : 10),
+         const Text("video 4")
+         const SizedBox(height : 10),
+         const Text("video 5")
+      ]
+    );
   }
 }
